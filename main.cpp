4a9fa9cb145f335b6dfbc6b2bc361f5cec2ddf2a
UnciaBit <ub.github@unciabit.com>
2022-01-11 23:10:12 +0900

    Working SQLite connection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
+#include "lib/sqlite3.h"
 
 using namespace std;
+
+int addBalance(double amount, char *label) {
+    cout << "amount: " << amount;
+    cout << "string: " << label;
+
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
+
+    return 0;
+}
+
 
 int main(int argc, char** argv) {
     // Command line budget calculator
@@ -18,10 +42,21 @@ int main(int argc, char** argv) {
 
     // [undo] finman undo -> undo last transaction
 
+    string action = argv[1];
 
-
-    cout << argc << endl;
-    cout << argv[1] << endl;
+    if (action == "add") {
+        addBalance(stod(argv[2]), argv[3]);
+    } else if (action == "sub") {
+        cout << "sub" << endl;
+    } else if (action == "new") {
+        cout << "new" << endl;
+    } else if (action == "undo") {
+        cout << "undo" << endl;
+    } else {
+        cout << "Invalid action" << endl;
+    }
 
     return 0;
 }
+
+
